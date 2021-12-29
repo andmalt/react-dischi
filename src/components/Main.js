79ff0b2a049418179883cb9bb449bea56ab608e2
andmalt <andrea.maltempi@gmail.com>
2021-12-29 18:06:40 +0100

    added call api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import Loading from './conditionals/Loading'
 import ErrorComponent from './conditionals/ErrorComponent'
+import axios from 'axios'
 import './Main.css'
 
 const Main = () => {
+    const [isLoading , setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+    const [discs, setDiscs] = useState([])
+
+    const url = 'https://flynn.boolean.careers/exercises/api/array/music';
+
+    const getDiscs = async () =>{
+        setIsLoading(true)
+        try {
+            const response = await axios.get(url)
+            setDiscs(response.data.response)
+            console.log(response.data.response);
+        } catch (error) {
+            setIsError(true)
+            console.log(error);
+        }
+        setIsLoading(false)
+        setIsError(false)
+    }
+
+    useEffect(() => {
+        getDiscs();
+    }, [])
+
+
     return (
         <div>
             
